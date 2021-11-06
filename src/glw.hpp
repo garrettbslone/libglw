b2303dae74eaf480435a20a65036d05433370afd
Garrett Slone <gsloneca@gmail.com>
2021-11-05 19:19:44 -0700

    glw: add graphics headers

--- src/glw.hpp
+++ src/glw.hpp
@@ -1,6 +1,22 @@
 #ifndef GLW_HPP
 #define GLW_HPP
 
+#include "core/app.hpp"
+
+#include "graphics/opengl/gl_buffer.hpp"
+#include "graphics/opengl/gl_framebuffer.hpp"
+#include "graphics/opengl/gl_shader.hpp"
+#include "graphics/opengl/gl_texture.hpp"
+#include "graphics/opengl/gl_vertex_array.hpp"
+
+#include "graphics/buffer.hpp"
+#include "graphics/color.hpp"
+#include "graphics/framebuffer.hpp"
+#include "graphics/shader.hpp"
+#include "graphics/texture.hpp"
+#include "graphics/vertex_array.hpp"
+#include "graphics/window.hpp"
+
 #include "math/vec.hpp"
 #include "math/vec2.hpp"
 #include "math/vec3.hpp"
@@ -8,6 +24,7 @@
 #include "math/mat.hpp"
 #include "math/mat3.hpp"
 #include "math/mat4.hpp"
+
 #include "util/exception.hpp"
 #include "util/math.hpp"
 
