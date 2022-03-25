7adb526bb101f7c8ebca2bc480a5accf5094ef0e
ivan776 <mendozaivan1224@gmail.com>
2022-03-24 23:00:36 -0700

    framebuffer.cpp and framebuffer.hpp comments

--- graphics/framebuffer.cpp
+++ graphics/framebuffer.cpp
@@ -9,6 +9,12 @@ namespace glw {
 
 framebuffer *framebuffer::create()
 {
+    /*
+     * The target parameter for this object can take one of 3 values: GL_FRAMEBUFFER, GL_READ_FRAMEBUFFER, or GL_DRAW_FRAMEBUFFER.
+     * In this case it is gl_framebuffer. This target simply sets both the read and write to the same FBO.
+     */
+
+    //Creates new framebuffer object
     return new gl_framebuffer();
 }
 
--- graphics/framebuffer.hpp
+++ graphics/framebuffer.hpp
@@ -8,13 +8,19 @@
 #include <cstdint>
 
 #include "color.hpp"
-
+/**
+ * This is a framebuffer class.
+ * The purpose of a framebuffer is to store buffers for internal rendering.
+ * A framebuffer is memory that stores color values for each pixel on display.
+ */
 namespace glw {
 
 typedef void (*framebuffer_size_cb)(int, int);
 
 class framebuffer {
+// public declarations of class elements
 public:
+    //Destroys object and allocated resources.
     virtual ~framebuffer() = default;
 
     virtual void init() = 0;
@@ -27,17 +33,33 @@ public:
 
     virtual void resize(uint32_t width, uint32_t height) = 0;
 
+    /*
+     * Getters and setters for width and height
+     * needed for framebuffer
+     */
     inline unsigned int get_width() const { return width; }
     inline unsigned int get_height() const { return height; }
 
+    /*
+     * Setters for resizing window. Callback function
+     */
     inline void set_resize_cb(framebuffer_size_cb cb) { this->fb_cb = cb; }
+
+    /*
+     * Setter for setting a color
+     */
     inline void set_clear_color(const color& clr) { this->clear_clr = clr; }
 
+    // Creates framebuffer
     static framebuffer *create();
 
+// declaration of protected class elements
 protected:
+    // color object
     color clear_clr;
+    //framebuffer size object
     framebuffer_size_cb fb_cb;
+    //width and height
     unsigned int width, height;
 };
 
